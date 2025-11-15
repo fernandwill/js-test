a99744308e3cb554b2571d6a73726674959d647e
Fernand William <156734117+fernandwill@users.noreply.github.com>
2025-11-15 17:28:26 +0700

    learned optional chaining

---
+++ optional-chaining.js
@@ -0,0 +1,11 @@
+const person = {
+    name: "Yui",
+    skills: {
+        combat: "Yowai",
+        driving: "Yowai"
+    }
+};
+
+console.log(person.skills.combat);
+console.log(person.skills?.stealth);
+console.log(person.weapon?.model);
